1858ec58b2bb2eff05fa405b93b16f69cb1ccd0c
heeneeee <gmldnjd0614@gmail.com>
2023-12-05 21:38:09 +0900

    Feat : dev pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import GlobalStyles from 'GlobalStyle';
-import React, { useEffect } from 'react';
-import { userStateChange } from 'config/firebase';
-import { useLoggedIn } from 'hooks/useAuth';
+import React from 'react';
 import Router from 'Router';
-
 export function App() {
-  const { setLoginState } = useLoggedIn();
-
-  useEffect(() => {
-    userStateChange((user) => setLoginState(user));
-  }, [setLoginState]);
-
   return (
     <div>
       <>
--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Layout from 'components/Layout/Layout';
+// import Layout from 'components/Layout/Layout';
 import MainPage from 'pages/MainPage';
 import MyPage from 'pages/MyPage';
 import LoginPage from 'pages/LoginPage';
@@ -8,13 +8,13 @@ import LoginPage from 'pages/LoginPage';
 export default function Router() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/loginpage" element={<LoginPage />} />
-        </Routes>
-      </Layout>
+      {/* <Layout> */}
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/loginpage" element={<LoginPage />} />
+      </Routes>
+      {/* </Layout> */}
     </BrowserRouter>
   );
 }
