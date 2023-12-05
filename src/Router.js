e6d0a65c0cf96c3c90c23ff97de5ddb61848f7de
HIITMEMARIO <alswnsgur@nate.com>
2023-12-05 19:59:28 +0900

    초기 세팅 끝

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from 'components/Layout/Layout';
+import MainPage from 'pages/MainPage';
+import MyPage from 'pages/MyPage';
+import LoginPage from 'pages/LoginPage';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Layout>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/loginpage" element={<LoginPage />} />
+        </Routes>
       </Layout>
     </BrowserRouter>
   );
