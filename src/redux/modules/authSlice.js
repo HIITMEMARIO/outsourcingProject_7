966fdb590b7fc4604521f2b498f34f14c68cda54
coldbrew <minsuk815@naver.com>
2023-12-05 21:33:43 +0900

    fix

---
+++ src/redux/modules/authSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit"
+const initialState = true
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state = true
+        }
+    }
+})
+export default authSlice.reducer
+export const { login } = authSlice.actions
