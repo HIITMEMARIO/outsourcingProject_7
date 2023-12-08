c1168087ee741a694b7acf51f91175bf2fe89548
coldbrew <minsuk815@naver.com>
2023-12-08 16:54:48 +0900

    test

--- src/components/Mypage/MyProfile.jsx
+++ src/components/Mypage/MyProfile.jsx
@@ -23,7 +23,7 @@ export default function MyProfile() {
   const myReview = review.filter((item) => {
     // console.log('user', item.userid);
     // console.log(nickname);
-    return item.userId === nickname;
+    return item.nickname === nickname;
   });
   console.log('myrevvvvvvvvvvvview', myReview);
   useEffect(() => {
@@ -93,7 +93,7 @@ export default function MyProfile() {
         </div>
 
         <StReviewContainer>
-          {myReview?.map((item) => {
+          {myReview.map((item) => {
             return (
               <div key={item.id}>
                 <div style={{ marginLeft: '550px', marginBottom: '10px' }}>
